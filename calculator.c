f3835df449a3c786c41b7a08b4caf324795d8f19
doer08736 <email.mygithub@gmail.com>
2023-08-20 20:22:09 +0530

    replace default flags with G_APPLICATION_REPLACE in GtkApplication creation

--- calculator.c
+++ calculator.c
@@ -206,8 +206,9 @@ void activate(GtkApplication *app, gpointer user_data){
 int main(int argc, char **argv){
     int status;
     gtk_init(&argc, &argv);
+	
+    GtkApplication *app = gtk_application_new("mainstuff.gtk", G_APPLICATION_REPLACE);
 
-    GtkApplication *app = gtk_application_new("mainstuff.gtk", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
     status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
